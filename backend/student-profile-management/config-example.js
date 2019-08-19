e5763064afa8a7d9715ec7edcb764c4ca91f69dc
Kailashnath Nagendran <kailashnath1998@gmail.com>
2019-08-19 19:05:14 +0530

    Added config.js
    Signed-off-by: Kailashnath Nagendran <kailashnath1998@gmail.com>

---
+++ backend/student-profile-management/config-example.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'secret': 'secrettoken',
+  'secure': false,
+  'domain': 'http://localhost:3000',
+  'superusers': [{
+    username: 'knn',
+    password: 'fabUz',
+    name: 'Kailashnath N',
+    phoneNumber: '9566911948',
+    email: 'kailashnath1998@gmail.com',
+  }, {
+    username: 'akash',
+    password: 'vaRPS',
+    name: 'Akash Ravi',
+    phoneNumber: '9003444090',
+    email: 'akashkravi@gmail.com',
+  }],
+  'emailConfig': {
+    username: '<mail-id>',
+    password: '<password>',
+    host: '<host>',
+    port: port,
+    secure: false
+  },
+  'notifyme': ['kailashnath1998@gmail.com', 'akashkravi@gmail.com'],
+
+};
